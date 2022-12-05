948767f74a4c233b0efaae9d8ed67e85b7751047
Nils Thielen <nt@kilograde.com>
2022-12-05 22:39:20 +0100

    03_01 and 03_02 solved and refactored a bit

--- 03_01/03_01.py
+++ 03_01/03_01.py
@@ -1,1 +1,22 @@
-with open('input.txt') as rucksack:
+letters = [chr(num) for num in range(97,97 + 26)]
+letters += [chr(num) for num in range(65,65 + 26)]
+
+with open('input.txt') as document:
+    priorities  = 0
+
+    for rucksack in document.read().splitlines():
+        firstCompartment = [letter for letter in rucksack[:int(len(rucksack)/2)]]
+        secondCompartment = [letter for letter in rucksack[int(len(rucksack)/2):]]
+    
+        print(firstCompartment)
+        print(secondCompartment)
+    
+        for i in firstCompartment:
+            for j in secondCompartment:
+                if i == j:
+                    priorities += letters.index(i) + 1
+                    break
+            else: continue
+            break
+
+    print(priorities)
---
+++ 03_02/03_02.py
@@ -0,0 +1,25 @@
+letters = [chr(num) for num in range(97,97 + 26)]
+letters += [chr(num) for num in range(65,65 + 26)]
+
+document = open('input.txt').read().splitlines()
+groupSize = 3
+groups = [document[n:n+groupSize] for n in range(0,len(document), groupSize)]
+
+priorities  = 0
+
+
+for group in groups:
+    for letter in group[0]:
+        matchCount = 0
+
+        for i in range(1, groupSize):
+            
+            if letter in group[i]:
+                matchCount += 1
+        if matchCount == groupSize - 1:
+            priorities += letters.index(letter) + 1
+            break
+        
+        
+
+print(priorities)
