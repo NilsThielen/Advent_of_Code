0471a1e16912b1c3faa4dc32b39e82f5c0a8fd90
Nils Thielen <nt@kilograde.com>
2022-12-05 14:35:58 +0100

    01_02 complete and renamed some variables

---
+++ 01_02/01_02.py
@@ -0,0 +1,12 @@
+with open('calories.txt') as caloriesList:
+    calories = []
+    document = caloriesList.read()
+
+    for entry in document.split('\n\n'):
+        calorieSum = 0
+        for line in entry.split('\n'):
+            calorieSum += int(line)
+        calories.append(calorieSum)
+
+    calories.sort()
+    print(sum(calories[-3:]))
